7e11c5923fa9a89c866917142c52d536b2a57f52
draganet <3417237276@qq.com>
2024-10-16 19:03:11 +0800

    loading wip

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <p>Customer Page </p>
+export default async function Page() {
+
+  const getNumber = new Promise<number>((resolve) => {
+    setTimeout(() => {
+      resolve(10)
+    }, 3000)
+  })
+
+  const number = await getNumber
+
+  return <p>Customer Page -- {number}</p>
+
 }
